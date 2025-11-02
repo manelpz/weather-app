49f3da75d5114facc7a9a55f54917f30012f44b7
Emmanuel Lopez Guerrero <manelpz@icloud.com>
2025-11-02 01:35:10 -0600

    adding typography and cityinfo card

---
+++ src/components/CityInfo/CityInfo.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import 'typeface-roboto'
+import CityInfo from './CityInfo'
+   
+export default {
+  title: 'CityInfo',
+  component: CityInfo
+};
+
+export const CityExample = () => (<CityInfo city="CDMX" country="Mexico" />);
+
